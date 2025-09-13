005df901639e8d0e6a9e1b39c68214e3e52afb13
Luke <lukedaird@gmail.com>
2025-09-12 22:02:59 -0700

    added rotation without kicks and simple block splitting

--- endless.gd
+++ endless.gd
@@ -1,19 +1,25 @@
 extends Node2D
 
-var board_width := 6
-var board_height := 8
+@export var board_width := 6
+@export var board_height := 11
 var cell_size := 64
 var board_start_position := Vector2i(0, 0)
-var spawn_location := Vector2i(2, 1)
+@export var spawn_location := Vector2i(2, 1)
 
 var grid : Array
 
-
 var seed_block : Node2D
 var stem_block : Node2D
+var rotation_state := 0
 
 var gravity_timer : Timer
 @export var gravity_time := 1.0
+
+var chain_timer : Timer
+@export var chain_time := 0.5
+
+var drop_timer : Timer
+@export var drop_time := 0.5
 
 var in_chain := false
 
@@ -36,6 +42,20 @@ func _ready() -> void:
 	gravity_timer.timeout.connect(_on_gravity_timer_timeout)
 	gravity_timer.start()
 
+	chain_timer = Timer.new()
+	add_child(chain_timer)
+	chain_timer.wait_time = chain_time
+	chain_timer.one_shot = true
+	chain_timer.timeout.connect(on_chain_timer_timeout)
+	chain_timer.start()
+
+	drop_timer = Timer.new()
+	add_child(drop_timer)
+	drop_timer.wait_time = drop_time
+	drop_timer.one_shot = true
+	drop_timer.timeout.connect(_on_drop_timer_timeout)
+	drop_timer.start()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left") :
 		var new_seed_location = seed_block.location + Vector2i(-1, 0)
@@ -49,6 +69,33 @@ func _input(event: InputEvent) -> void:
 		if is_valid_move(new_seed_location) and is_valid_move(new_stem_location) :
 			move_block(seed_block, new_seed_location)
 			move_block(stem_block, new_stem_location)
+	elif event.is_action_pressed("ui_accept") :
+		rotate_falling_blocks(false)
+	elif event.is_action_pressed("ui_cancel") :
+		rotate_falling_blocks(true)
+
+func rotate_falling_blocks(clockwise := true) :
+	var desired_rotation_state : int
+	if clockwise :
+		desired_rotation_state = wrap(rotation_state + 1, 0, 4)
+	else :
+		desired_rotation_state = wrap(rotation_state - 1, 0, 4)
+	
+	var stem_vector : Vector2i
+	match desired_rotation_state :
+		0 :
+			stem_vector = Vector2i(0, -1)
+		1 :
+			stem_vector = Vector2i(1, 0)
+		2 :
+			stem_vector = Vector2i(0, 1)
+		3 :
+			stem_vector = Vector2i(-1, 0)
+	
+	if is_valid_move(seed_block.location + stem_vector) :
+		rotation_state = desired_rotation_state
+		move_block(stem_block, seed_block.location + stem_vector)
+		
 
 func location_to_position(location: Vector2i) -> Vector2i:
 	return board_start_position + Vector2i((0.5+location.x)*cell_size, (0.5+location.y)*cell_size)
@@ -66,8 +113,22 @@ func _on_gravity_timer_timeout() :
 	gravity_timer.start()
 
 func place_piece() :
+	var dropping_seed_block = seed_block
+	var dropping_stem_block = stem_block
 	seed_block = null
 	stem_block = null
+	
+	var test_location : Vector2i
+	test_location = dropping_seed_block.location + Vector2i(0, 1)
+	while is_valid_move(test_location) :
+		move_block(dropping_seed_block, test_location)
+		test_location += Vector2i(0, 1)
+	
+	test_location = dropping_stem_block.location + Vector2i(0, 1)
+	while is_valid_move(test_location) :
+		move_block(dropping_stem_block, test_location)
+		test_location += Vector2i(0, 1)
+	
 	spawn_blocks()
 	pass #turn falling pieces into real pieces, check for chain
 
@@ -85,6 +146,8 @@ func check_for_chain() -> bool :
 	return false
 
 func spawn_blocks() -> void :
+	rotation_state = 0
+	
 	if is_valid_move(spawn_location) and is_valid_move(spawn_location + Vector2i(0, -1)) :
 		seed_block = block.instantiate()
 		seed_block.change_color(randi_range(0, 2))
